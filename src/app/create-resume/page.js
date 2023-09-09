1dad723754fa251d23a0cf1b7ea09a4ba4cdde3e
DinaSZH <dinasaginbayeva21@gmail.com>
2023-09-10 01:09:25 +0600

    added component AutoComplete

--- src/app/create-resume/page.js
+++ src/app/create-resume/page.js
@@ -4,12 +4,22 @@ import Header from '../../components/header'
 import Input from '@/components/input';
 import { END_POINT } from '@/config/end-point';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
+import AutoCompleteSelect from '@/components/AutoCompleteSelect';
 
 export default function CreateResume() {
+    const [cities, setCities] = useState([]);
+    useEffect(() => {
+        console.log("didMount")
+        axios.get(`${END_POINT}/api/region/cities`).then(res => {
+            setCities(res.data);
+        })
+    }, [])
 
-    axios.get(`${END_POINT}/api/region/cities`).then(res => {
-        console.log(res)
-    })
+   console.log("rerender")
+   const onSelect = (data) => {
+    console.log("onSelect", data)
+   }
 
 
   return (
@@ -22,7 +32,12 @@ export default function CreateResume() {
         <Input placeholder="" type="text" label="Имя" size="fieldset-md" />
         <Input placeholder="" type="text" label="Фамилия" size="fieldset-md" />
         <Input placeholder="" type="text" label="Мобильный телефон" size="fieldset-md" />
+        <AutoCompleteSelect placeholder="" type="text" label="Город проживания" size="fieldset-md" items={cities} onSelect={onSelect}/>
+
+        <h3>Основная информация</h3>
+        
       </div>
     </main>
   )
 }
+
