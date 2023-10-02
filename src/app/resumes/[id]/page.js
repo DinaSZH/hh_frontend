1e087ed91938bda3da0501896852090fd5ece6d5
DinaSZH <dinasaginbayeva21@gmail.com>
2023-10-02 22:43:45 +0600

    fetch resume data from backend

--- src/app/resumes/[id]/page.js
+++ src/app/resumes/[id]/page.js
@@ -2,8 +2,21 @@
 import Header from '@/components/header'
 import MyResumes from '@/components/myresumes'
 import Link from 'next/link'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {getResumeById} from '@/app/store/slices/resumeSlice'
+import { useParams } from 'next/navigation'
 
 export default function ResumePage() {
+
+    const dispatch = useDispatch();
+    const {id} = useParams();
+    const resume = useSelector(state => state.resume.resume)
+
+    const didMount = () => {
+        dispatch(getResumeById(id))
+    }
+    useEffect(didMount, [])
 
   return (
     <main>
--- src/app/store/slices/resumeSlice.js
+++ src/app/store/slices/resumeSlice.js
@@ -6,7 +6,8 @@ import { END_POINT } from '@/config/end-point'
 export const resumeSlice = createSlice({
   name: 'resume',
   initialState: {
-    resumes: []
+    resumes: [],
+    resume: {}
   },
   reducers: {
     setMyResumes: (state, action) => {
@@ -14,12 +15,15 @@ export const resumeSlice = createSlice({
     },
     uppendResume: (state, action) => {
       state.resumes = [...state.resumes, action.payload.newresume]
+    },
+    setResume: (state, action) => {
+      state.resume = action.payload.resume
     }
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { setMyResumes, uppendResume } = resumeSlice.actions
+export const { setMyResumes, uppendResume, setResume } = resumeSlice.actions
 
 export const getMyResumes = () => async (dispatch) => {
   try{
@@ -27,8 +31,16 @@ export const getMyResumes = () => async (dispatch) => {
     dispatch(setMyResumes({resumes: res.data}))
   } catch(e) {
     alert("Что то пошло не так, сообщите об ошибке Тех спецам сайта!")
-  }
-      
+  }  
+}
+
+export const getResumeById = (id) => async (dispatch) => {
+  try{
+    const res = await axios.get(`${END_POINT}/api/resume/${id}`);
+    dispatch(setResume({resume: res.data}))
+  } catch(e) {
+    alert("Что то пошло не так, сообщите об ошибке Тех спецам сайта!")
+  }  
 }
 
 export const createResume = (sendData, router) => async (dispatch) => {
